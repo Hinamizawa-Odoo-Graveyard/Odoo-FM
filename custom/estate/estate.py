97b56c47a6b52c1bf93de8c720699c0df6c17cd7
Hinamizawa <themoonbunnys@gmail.com>
2022-07-06 13:22:57 -0400

    Update estate.py

--- custom/estate/estate.py
+++ custom/estate/estate.py
@@ -4,13 +4,24 @@
 from odoo import fields, models
 
 
-class real_estate_management(models.Model):
-    _name = "real.estate.management"
+class estate_property(models.Model):
+    _name = "estate.property"
     _description = "real estate plans"
     _order = "sequence"
 
     name = fields.Char('Estate Name', required=True)
-    number_of_months = fields.Integer('# Months', required=True)
+    postcode = fields.Char(string="Postcode", required=True)
+    date_availability = fields.Date(string="Date Available", required=True)
+    expected_price = fields.Float(string="Expected Price", required=True)
+    selling_price = fields.Float(string="Selling Price", required=True)
+    bedrooms = fields.Integer(string="Bedrooms", required=True)
+    living_area = fields.Float(string="Living Area", required=True)
+    facades = fields.Integer(string="Facades", required=True)
+    garage = fields.Boolean(string="Garage", required=True)
+    garden = fields.Boolean(string="Garden", required=True)
+    garden_area = fields.Integer(string="Garden Area", required=True)
+    garden_orientation = fields.Selection(
+        'North, South, East, West', string="Garden Orientation", required=True)
     active = fields.Boolean('Active', default=True)
     sequence = fields.Integer('Sequence', default=10)
 
