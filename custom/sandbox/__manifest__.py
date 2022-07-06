5cd737834c3bfa8e10be85520757fdf2b21f01da
Hinamizawa <themoonbunnys@gmail.com>
2022-07-06 10:11:01 -0400

    u

---
+++ custom/sandbox/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sandbox",  # Module title
+    'summary': "Manage books easily",  # Module subtitle phrase
+    'description': """
+Manage Estates
+==============
+Description related to Real Estate.
+    """,  # Supports reStructuredText(RST) format
+    'author': "Hinamizawa",
+    'website': "http://www.example.com",
+    'category': 'Tools',
+    'version': '14.0.1',
+    'depends': ['base'],
+    # This data files will be loaded at the installation (commented because file is not added in this example)
+    # 'data': [
+    #     'views.xml'
+    # ],
+    # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
+    # 'demo': [
+    #     'demo.xml'
+    # ],
+}
