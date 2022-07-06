db9dcbc9a294ce803571434f126b340d99ea7b41
Hinamizawa <themoonbunnys@gmail.com>
2022-07-06 13:10:31 -0400

    Update __manifest__.py

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -17,12 +17,11 @@ Description related to Real Estate.
     'application': True,
     'auto_install': False
 }
-    # This data files will be loaded at the installation (commented because file is not added in this example)
-    # 'data': [
-    #     'views.xml'
-    # ],
-    # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
-    # 'demo': [
-    #     'demo.xml'
-    # ],
-}
+# This data files will be loaded at the installation (commented because file is not added in this example)
+# 'data': [
+#     'views.xml'
+# ],
+# This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
+# 'demo': [
+#     'demo.xml'
+# ],}
